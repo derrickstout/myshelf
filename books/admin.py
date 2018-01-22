a8460d8a36eef3c7fe840c41f5aa133eef9b1f8e
derrickstout <derrickstout@outlook.com>
2018-01-21 23:26:05 -0500

    Remove Audiobook and Narrator, simplify Admin. Rethink things.

--- books/admin.py
+++ books/admin.py
@@ -1,23 +1,27 @@
 from django.contrib import admin
-from .models import (Author, Book, Narrator, Audiobook)
+from .models import (Author, Book)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
 	fieldsets = [
-		('Details', {'fields': ['title','description', 'authors', 'series']}),
-		('User Input', {'fields': ['review', 'finished']}),
+		('Details', {'fields': ['title','description','book_type', 'authors', 'series']}),
+		('User Input', {'fields': ['review', 'finished',]}),
 	]
-	list_display = ("title", "series", "finished")
+	list_display = ("title", "list_authors", "series", "book_type", "finished",)
 
-	list_editable = ("finished",)
+	list_editable = ("finished", "book_type",)
 
-@admin.register(Audiobook)
-class AudiobookAdmin(admin.ModelAdmin):
-	fieldsets = [
-		('Details', {'fields': ['title','description', 'authors', 'narrator', 'series']}),
-		('User Input', {'fields': ['review', 'finished']}),
-	]
+	list_filter = ("finished", "authors")
+
+
+# Removing this model for now as it seems redundant for what I want to do
+# @admin.register(Audiobook)
+# class AudiobookAdmin(admin.ModelAdmin):
+# 	fieldsets = [
+# 		('Details', {'fields': ['title','description', 'authors', 'narrator', 'series']}),
+# 		('User Input', {'fields': ['review', 'finished']}),
+# 	]
 
 # Register your models here.
 
-admin.site.register([Author, Narrator])
+admin.site.register([Author])
--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -8,12 +9,29 @@ class Author(models.Model):
 		return self.name
 
 class Book(models.Model):
+	PRINT = 'PRINT'
+	AUDIO = 'AUDIO'
+	EBOOK = 'EBOOK'
+	BOOK_TYPE_CHOICES = (
+		(PRINT, 'Print Book'),
+		(AUDIO, 'Audiobook'),
+		(EBOOK, 'eBook'),
+		)
+	#There has to be a better way to do this than multiple choice
+	# NEXT_THREE_RANK_CHOICES	= (
+	# 	(1, '1'),
+	# 	(2, '2'),
+	# 	(3, '3'),
+	# 	)
+	# next_three_rank = models.CharField(max_length=1, choices=NEXT_THREE_RANK_CHOICES, default='null', blank=True, null=True)
+	book_type = models.CharField(max_length=5, choices=BOOK_TYPE_CHOICES, default='AUDIO')
 	title = models.CharField(max_length=150)
 	description = models.TextField(blank=True, null=True)
 	series = models.CharField(max_length=150, blank=True, null=True)
 	authors = models.ManyToManyField(Author, blank=True)
 	review = models.TextField(blank=True, null=True)
 	finished = models.BooleanField(default=False, verbose_name="Finished")
+	# own = models.BooleanField(default=False)
 	# image = models.ImageField()
 
 	def __str__(self):
@@ -22,15 +40,17 @@ class Book(models.Model):
 	def list_authors(self):
 		return ", ".join([author.name for author in self.authors.all()])
 
-class Narrator(models.Model):
-	narrator = models.CharField(max_length=80)
 
-	def __str__(self):
-		return self.narrator
+# Removing to simplify, but saving in case I decide it makes sense later on
+# class Narrator(models.Model):
+# 	narrator = models.CharField(max_length=80)
 
-class Audiobook(Book):
-	length = models.CharField(max_length=20)
-	narrator = models.ForeignKey(Narrator, on_delete=models.SET_NULL, blank=False, null=True)
+# 	def __str__(self):
+# 		return self.narrator
 
-	def __str__(self):
-		return self.title
+# class Audiobook(Book):
+# 	length = models.CharField(max_length=20)
+# 	narrator = models.ForeignKey(Narrator, on_delete=models.SET_NULL, blank=False, null=True)
+
+# 	def __str__(self):
+# 		return self.title

d37bffd6ac9680cf2ba539d9af54d56800f4f826
derrickstout <derrickstout@outlook.com>
2018-01-25 00:40:01 -0500

    Create index template and very basic views for index and book detail page

--- books/views.py
+++ books/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Is anyone there?")
+	book_list = Book.objects.all()
+	# book_list = ', '.join([b.title for b in books])
+	context = {
+	'book_list': book_list
+	}
+	return render(request, 'books/index.html', context)
+
+def detail(request, book_id):
+	book_detail = (Book.objects.get(pk=book_id).title, " by ", Book.objects.get(pk=book_id).list_authors())
+
+	return HttpResponse(book_detail)
